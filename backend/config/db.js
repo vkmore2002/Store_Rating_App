49d945d60ba372c33f1c98f0c57662ac8f1b7ba2
vkmore2002 <vivekmore153@gmail.com>
2026-04-07 12:51:03 +0530

    db changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,12 +6,10 @@ dotenv.config();
 // Create connection pool (better than single connection)
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT, // IMPORTANT
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
 
 // Test connection and handle errors
